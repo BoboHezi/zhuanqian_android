ad17b038b72179660ed581dafbdefefe57d8e224
BoboHezi <eliflichang@gmail.com>
2021-08-13 18:03:50 +0800

    add navigation

--- app/src/main/java/com/ewq/zq/activity/MainActivity.java
+++ app/src/main/java/com/ewq/zq/activity/MainActivity.java
@@ -1,9 +1,27 @@
 package com.ewq.zq.activity;
 
+import android.content.res.ColorStateList;
+import android.os.Build;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.ewq.zq.R;
 import com.ewq.zq.base.BaseActivity;
+import com.ewq.zq.widget.NavigationLayout;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class MainActivity extends BaseActivity implements NavigationLayout.OnTabClickListener {
+
+    @BindView(R.id.main_pager)
+    ViewPager2 mViewPager;
+    @BindView(R.id.nav_tab)
+    NavigationLayout mNavigation;
 
     @Override
     protected int getLayoutId() {
@@ -12,11 +30,31 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        mNavigation.setTabClickListener(this);
+        updateNaviView(mNavigation, 0);
     }
 
     @Override
     protected void loadData() {
 
+    }
+
+    @Override
+    public void onTabClick(View view, int index) {
+        updateNaviView(mNavigation, index);
+    }
+
+    private void updateNaviView(NavigationLayout navigation, int select) {
+        for (int i = 0; i < navigation.getChildCount(); i++) {
+            ImageView icon = navigation.getChildAt(i).findViewById(R.id.nav_icon);
+            TextView text = navigation.getChildAt(i).findViewById(R.id.nav_text);
+            int color = getColor(select == i ? R.color.holo_red_light : R.color.material_grey_850);
+            if (icon != null) {
+                icon.setImageTintList(ColorStateList.valueOf(color));
+            }
+            if (text != null) {
+                text.setTextColor(color);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ewq/zq/widget/NavigationLayout.java
@@ -0,0 +1,173 @@
+package com.ewq.zq.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.ewq.tools.common.Constant;
+import com.ewq.tools.info.SharedInfo;
+import com.ewq.zq.R;
+
+import eli.avocado.utils.StringUtils;
+
+public class NavigationLayout extends LinearLayout implements View.OnClickListener, ViewPager.OnPageChangeListener, View.OnLongClickListener, View.OnTouchListener {
+
+    private ViewPager mViewPager;
+    private OnTabClickListener tabClick;
+
+    private GestureDetector gestureDetector;
+
+    private Context context;
+
+    public NavigationLayout(Context context) {
+        this(context, null);
+    }
+
+    public NavigationLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NavigationLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        gestureDetector = new GestureDetector(context, new QYGestureDetector());
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.setOnClickListener(this);
+            child.setOnTouchListener((v, event) -> {
+                if (gestureDetector != null) {
+                    gestureDetector.onTouchEvent(event);
+                }
+                return false;
+            });
+        }
+    }
+
+    public void setTabClickListener(OnTabClickListener listener) {
+        tabClick = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (shake(v)) {
+            return;
+        }
+        int index = indexOfChild(v);
+        if (context != null && StringUtils.isEmpty((String) SharedInfo.getInstance()
+                .getValue(Constant.userId, ""))) {
+            // AppManager.getInstance().jumpActivity(context, LoginCodeActivity.class);
+            return;
+        }
+
+        if (mViewPager != null) {
+            mViewPager.setCurrentItem(index, true);
+        }
+
+        if (tabClick != null) {
+            tabClick.onTabClick(v, index);
+        }
+    }
+
+    public void attachViewPager(ViewPager viewPager) {
+        mViewPager = viewPager;
+        mViewPager.addOnPageChangeListener(this);
+        select(viewPager.getCurrentItem());
+    }
+
+    public void locateTo(int position) {
+        if (position >= 0 && position < getChildCount()) {
+            View selected = getChildAt(position);
+            if (selected != null) {
+                selected.performClick();
+            }
+        }
+    }
+
+    /**
+     * 选中tab
+     */
+    public void select(int index) {
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            if (child instanceof ImageView) {
+                if (index == i) {
+                    child.setSelected(true);
+                } else {
+                    child.setSelected(false);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        select(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return false;
+    }
+
+    /**
+     * 防止重复点击 1秒 内允许点击一次
+     *
+     * @param view view
+     * @return boolean
+     */
+    private boolean shake(View view) {
+        Object lastTime = view.getTag(R.id.view_shake);
+        if (lastTime != null) {
+            long dx = System.currentTimeMillis() - (long) lastTime;
+            if (dx < 500) {
+                return true;
+            } else {
+                view.setTag(R.id.view_shake, System.currentTimeMillis());
+                return false;
+            }
+        } else {
+            view.setTag(R.id.view_shake, System.currentTimeMillis());
+            return false;
+        }
+    }
+
+    public interface OnTabClickListener {
+        void onTabClick(View view, int index);
+    }
+
+    class QYGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return super.onDoubleTap(e);
+        }
+    }
+}
